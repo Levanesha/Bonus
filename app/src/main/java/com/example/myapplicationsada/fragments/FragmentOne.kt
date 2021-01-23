65db8cdfb5b58b23998953a6a2601d5e219f7f92
Levanesha <74672817+Levanesha@users.noreply.github.com>
2021-01-23 04:06:29 +0400

    Delete app/src/main/java/com/example/myapplicationsada/fragments directory

--- app/src/main/java/com/example/myapplicationsada/fragments/FragmentOne.kt
+++
@@ -1,7 +0,0 @@
-package com.example.myapplicationsada.fragments
-
-import androidx.fragment.app.Fragment
-import com.example.myapplicationsada.R
-
-class FragmentOne:Fragment(R.layout.activity_one) {
-}
